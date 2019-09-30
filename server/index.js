2491787ded13153b5bdffbadc20a0af5d3a74a34
Pavel <pipetrovich@itertech.by>
2019-09-30 15:53:57 +0300

    server init

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-import express from 'express';
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || '3001');
+
+app.listen((error) => {
+    if (error) console.log(error);
+    else console.log(`Server start on ${app.get('port')} port`);
+});
