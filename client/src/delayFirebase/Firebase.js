6f1d6c3d7ccbc82183874894d7d3a0c70a91a801
Pavel Petrovich <gaara33377@gmail.com>
2019-10-13 19:30:44 +0300

    firebase init delay

---
+++ client/src/delayFirebase/Firebase.js
@@ -0,0 +1,60 @@
+import firebase from 'firebase/app';
+import firebaseConfig from './firebaseConfig';
+import config from '../config.json';
+const auth = require('firebase/auth');
+const firestore = require('firebase/firestore');
+
+class Firebase {
+
+    constructor(firebaseConfig){
+        /** @Init firebase */
+
+        firebase.initializeApp(firebaseConfig);
+        this.auth = firebase.auth();
+        this.db = firebase.firestore();
+
+        if (config.firebase.lowConnection === 'true'
+        && (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production'))
+        this.db.enablePersistence() /** if user use low internet @connection */
+            .then(res => console.warn(res))
+            .catch(er => console.error(er));
+    }
+
+    saveSession(rules){
+        /** @Set firebase type session */
+        return this.auth.setPersistence(firebase.auth.Auth.Persistence[rules]);
+    }
+
+    login(email, password){
+        /** @login firebase */
+        try {
+            return this.auth.signInWithEmailAndPassword(email, password);
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    registration(email, password){
+        /** @create new user */
+        return this.auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    signOut(){
+        /** @destroy session */
+        return this.auth.signOut()
+    }
+
+    getCurrentUser() {
+        /** @get current user */
+        return this.auth.currentUser;
+    }
+}
+let firebaseInterface = new Firebase(firebaseConfig);
+
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production')
+firebaseInterface.saveSession(config.firebase.session);
+
+export default firebaseInterface;
+export { auth, firestore };
+
--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,28 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
+
+import 'normalize.css';
 import 'antd/dist/antd.css';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import firebase from './delayFirebase/Firebase';
+import firebaseContext from './delayFirebase/firebaseContext'; /** firebase contect API */
 
 import store from './Redux/store';
 
 
 ReactDOM.render(
 <BrowserRouter basename = {'/'}>
-    <Provider store = {store}>
-        <App />
-    </Provider>
-</BrowserRouter>
-, 
+    <firebaseContext.Provider value = {firebase}>
+        <Provider store = {store}>
+            <App />
+        </Provider>
+    </firebaseContext.Provider>
+</BrowserRouter>, 
 document.getElementById('root'));
 
 serviceWorker.unregister();
