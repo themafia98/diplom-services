3b11ebb8bad8e3d3c6e19321be2a065979237fea
themafia98 <gaara33377@gmail.com>
2019-10-13 16:13:13 +0300

    antd init layout

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
