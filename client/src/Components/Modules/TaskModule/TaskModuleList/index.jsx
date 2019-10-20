696bb29d209c6c6b5307bea9a617570a3ebe4b21
Pavel Petrovich <gaara33377@gmail.com>
2019-10-20 19:11:09 +0300

    test

--- client/src/Components/Modules/TaskModule/TaskModuleList/index.jsx
+++ client/src/Components/Modules/TaskModule/TaskModuleList/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-
+import Scrollbars from "react-custom-scrollbars";
 import TableView from "../../../TableView";
 import TitleModule from "../../../TitleModule";
 
 class TaskModuleList extends React.PureComponent {
     render() {
         return (
-            <div className="taskModule_all">
-                <TitleModule additional="Все задачи" classNameTitle="taskModuleTittle" title="Список всех задач" />
-                <div className="taskModuleAll_main">
-                    <TableView path="searchTable" />
+            <Scrollbars>
+                <div className="taskModule_all">
+                    <TitleModule additional="Все задачи" classNameTitle="taskModuleTittle" title="Список всех задач" />
+                    <div className="taskModuleAll_main">
+                        <TableView path="searchTable" />
+                    </div>
                 </div>
-            </div>
+            </Scrollbars>
         );
     }
 }
