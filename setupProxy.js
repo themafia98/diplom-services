b493c067698c2c044ffdff918495cc7153a492ca
Pavel Petrovich <gaara33377@gmail.com>
2019-11-14 23:23:03 +0300

    config

--- setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require("http-proxy-middleware");
-module.exports = app => {
-    app.use(
-        proxy("asdsdasdasd/.netlify/functions/server/", {
-            target: "adasdasdsadasdsahttp://localhost:9000/"
-        })
-    );
-};
