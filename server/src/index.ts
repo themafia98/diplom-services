a7d50198bdfbb81bc26e52dfda1eb106ef058359
themafia98 <gaara33377@gmail.com>
2019-11-17 18:39:33 +0300

    types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
-
-import express, { Router, Request, Response } from "express";
+import express, { Router, Request, Response, Express } from "express";
 import path from 'path';
 import cluster from 'cluster';
 import os from "os";
@@ -8,7 +7,7 @@ import { Server } from "http";
 
 namespace Entrypoint {
 
-    const cpuLentgh = os.cpus().length;
+    const cpuLentgh: number = os.cpus().length;
 
     if (cluster.isMaster) {
 
@@ -18,28 +17,28 @@ namespace Entrypoint {
 
     } else {
 
-        const app = express();
+        const app: Express = express();
         app.disabled("X-Powered-By");
         app.use(express.static(process.cwd() + "/client/build"));
 
-        const router = express.Router();
+        const router: Router = express.Router();
 
         const port: string = process.env.PORT || "3001";
 
         app.set("port", port);
 
-        const server: Server = app.listen(port, () => {
+        const server: Server = app.listen(port, (): void => {
             console.log(`Worker ${process.pid} started`);
             console.log(`Server or worker listen on ${port}.`);
         });
 
         const route: Router = app.use("/rest", router);
 
-        route.get('*', (req, res) => {
+        route.get('*', (req: Request, res: Response): void => {
             res.sendFile(path.join(process.cwd() + '/client/build/index.html'));
         });
 
-        process.on("SIGTERM", () => {
+        process.on("SIGTERM", (): void => {
             server.close();
         })
     }
