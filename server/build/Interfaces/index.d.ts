bbc246e7f0e8810f919b714fd804f2efa7c7cc1f
Pavel Petrovich <gaara33377@gmail.com>
2019-11-20 00:42:50 +0300

    try

---
+++ server/build/Interfaces/index.d.ts
@@ -0,0 +1,10 @@
+import { Application, Router as RouteExpress } from "express";
+export interface ServerRun {
+    start(): void;
+}
+export interface Route {
+    getRest(): Application;
+    initInstance(path?: string): Application;
+    getEntrypoint(): Application;
+    createRoute(path: string, flag?: string): RouteExpress;
+}
