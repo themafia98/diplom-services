e99c9416d0d675e29355cd4d4d7a39f7f1579850
themafia98 <gaara33377@gmail.com>
2019-11-23 00:07:30 +0300

    ssl

--- client/src/Components/ContentView/index.jsx
+++ client/src/Components/ContentView/index.jsx
@@ -10,7 +10,7 @@ import MainModule from "../Modules/MainModule";
 import CabinetModule from "../Modules/CabinetModule";
 import TaskModule from "../Modules/TaskModule";
 import StatisticsModule from "../Modules/StatisticsModule";
-import SettingsModule from "../Modules/SettingsModule";
+import SettingsModule from "../Modules/settingsModule";
 import ContactModule from "../Modules/ContactModule";
 import CustomersModule from "../Modules/CustomersModule";
 
