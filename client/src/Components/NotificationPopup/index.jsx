eb2689dd40d2778a795bc90423d4a0fe40138117
Pavel Petrovich <gaara33377@gmail.com>
2019-11-24 20:06:26 +0300

    NotificationPopup

--- client/src/Components/NotificationPopup/index.jsx
+++ client/src/Components/NotificationPopup/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import Scrollbars from "react-custom-scrollbars";
 import { Icon, Badge, Popover } from "antd";
 
 class NotificationPopup extends React.PureComponent {
     render() {
         const content = (
-            <div>
-                <p>уведомление 1</p>
-                <p>уведомление 2</p>
+            <div style={{ height: "100px" }} className="notificationContent">
+                <Scrollbars>
+                    <p>уведомление 1</p>
+                    <p>уведомление 2</p>
+                    <p>уведомление 3</p>
+                    <p>уведомление 4</p>
+                    <p>уведомление 5</p>
+                    <p>уведомление 6</p>
+                </Scrollbars>
             </div>
         );
-        const counter = 1;
+        const counter = 6;
         return (
             <React.Fragment>
                 <div className="notificationControllers">
