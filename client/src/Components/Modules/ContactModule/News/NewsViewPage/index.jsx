89a78a106e96aac9bd3a824cc981dac5ca610953
Pavel Petrovich <gaara33377@gmail.com>
2019-12-07 16:27:57 +0300

    hook newsView

--- client/src/Components/Modules/ContactModule/News/NewsViewPage/index.jsx
+++ client/src/Components/Modules/ContactModule/News/NewsViewPage/index.jsx
@@ -1,51 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import _ from "lodash";
-import { connect } from "react-redux";
-import { openPageWithDataAction } from "../../../../../Redux/actions/routerActions";
-import { loadCurrentData } from "../../../../../Redux/actions/routerActions/middleware";
-import Loader from "../../../../Loader";
+import uuid from "uuid/v4";
+
+import { getEditorJSON } from "../../../../../Utils/schema";
 import TitleModule from "../../../../TitleModule";
 import EditorTextarea from "../../../../Textarea/EditorTextarea";
 
-class NewsViewPage extends React.PureComponent {
-    state = {
-        isLoading: true,
-        loading: true
-    };
-
-    render() {
-        const { listdata = null } = this.props;
-
-        return (
-            <div className="newsView-page">
-                <TitleModule
-                    classNameTitle="tittle_contactModule_pageNews"
-                    title={listdata && listdata._id ? listdata._id : "Новость"}
-                />
-                <div className="newsView-page__main">
-                    <EditorTextarea
-                        key={listdata ? listdata._id : null}
-                        readOnly={true}
-                        contentState={listdata ? listdata : null}
-                    />
-                </div>
+const NewsViewPage = ({ listdata = null }) => {
+    const [id] = useState(listdata ? listdata._id : uuid());
+    return (
+        <div className="newsView-page">
+            <TitleModule classNameTitle="tittle_contactModule_pageNews" title={listdata && id ? id : "Новость"} />
+            <div className="newsView-page__main">
+                <EditorTextarea key={id} readOnly={true} contentState={listdata ? listdata : getEditorJSON()} />
             </div>
-        );
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        router: state.router,
-        publicReducer: state.publicReducer
-    };
-};
-const mapDispatchToProps = dispatch => {
-    return {
-        onOpenPageWithData: data => dispatch(openPageWithDataAction(data)),
-        onLoadCurrentData: ({ path, storeLoad }) => dispatch(loadCurrentData({ path, storeLoad }))
-    };
+        </div>
+    );
 };
 
-export { NewsViewPage };
-export default connect(mapStateToProps, mapDispatchToProps)(NewsViewPage);
+export default NewsViewPage;
