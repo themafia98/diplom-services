26d19506a3e94ef830400cf8a70f2030e34dbc9d
Pavel Petrovich <gaara33377@gmail.com>
2019-12-13 20:38:24 +0300

    color

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import cluster, { Worker } from "cluster";
+import chalk from "chalk";
 import fs from "fs";
 import os from "os";
 import { ServerRun } from "./Utils/Interfaces";
@@ -17,13 +18,14 @@ namespace Entrypoint {
             cluster.fork();
 
             cluster.on("exit", (worker: Worker, code: number, signal: string) => {
-                console.log(`worker ${worker.process.pid} exit.`);
+                console.log(`${chalk.yellow("worker")} ${chalk.red(worker.process.pid)} exit.`);
 
                 const child = cluster.fork();
-                console.log(`New worker ${child.process.pid} born.`);
+                console.log(`New ${chalk.yellow("worker")} ${chalk.red(child.process.pid)} born.`);
             });
         }
     } else {
+        throw "sdas";
         try {
             const server: ServerRun = new ServerRunner(process.env.APP_PORT || "3001");
             server.start();
