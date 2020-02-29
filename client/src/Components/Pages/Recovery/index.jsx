da62a045aab10d31a0463ec9cd35ee07ff032c35
themafia98 <gaara33377@gmail.com>
2020-03-01 02:00:12 +0300

    recovory

--- client/src/Components/Pages/Recovery/index.jsx
+++ client/src/Components/Pages/Recovery/index.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { Button, Input } from "antd";
 
 
 const Recovory = props => {
 
+    const [status, setStatus] = useState(null);
     const [mode, setMode] = useState("emailMode");
 
-    const onSubmit = event => {
+    const onSubmit = async event => {
+        const recovoryData = [];
 
+        axios.post("/rest/recovory", recovoryData).then(res => {
+            if (res.status !== 200) throw new Error("Bad data for recovory");
+
+            setStatus("Новый пороль будет выслан вам на почту.");
+
+        }).catch(error => {
+            console.error(error);
+            setStatus(error);
+        });
     };
 
     const onChangeMode = event => {
@@ -23,10 +35,11 @@ const Recovory = props => {
         <div className="recovory">
             <form className="recovory_form" name="recovoryForm">
                 <p>Восстоновление доступа в систему</p>
+                {status ? <span className="status">{status}</span> : null}
                 <Input
                     type="text"
                     placeholder={mode === "emailMode"
-                        ? "введите вашу электронную почту"
+                        ? "введите ваш email"
                         : "введите ваш логин"
                     }
                 />
