7391572168da73a29cf10e12a62e3bc2c881bb27
themafia98 <gaara33377@gmail.com>
2020-03-13 22:43:23 +0300

    refac

--- client/src/Components/ModalWindow/RegistrationModal/index.jsx
+++ client/src/Components/ModalWindow/RegistrationModal/index.jsx
@@ -1,41 +1,35 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Input, Select } from "antd";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Input, Select } from 'antd';
 const { Option } = Select;
 const RegistrationModal = ({ cbOnChangeSelect, cbOnChange }) => (
-    <div>
-        <Input name="email" onChange={cbOnChange} className="email" type="text" size="default" placeholder="email" />
-        <Input
-            name="password"
-            onChange={cbOnChange}
-            className="password"
-            type="password"
-            size="default"
-            placeholder="password"
-        />
-        <Input name="name" onChange={cbOnChange} className="name" type="text" size="default" placeholder="name" />
-        <Input
-            name="surname"
-            onChange={cbOnChange}
-            className="surname"
-            type="text"
-            size="default"
-            placeholder="surname"
-        />
-        <Select
-            onChange={cbOnChangeSelect}
-            className="selectDepartament"
-            placeholder="Select a departament"
-            optionFilterProp="depart"
-        >
-            <Option value="Admin">Admin</Option>
-            <Option value="Doctor">Doctor</Option>
-        </Select>
-    </div>
+  <div>
+    <Input name="email" onChange={cbOnChange} className="email" type="text" size="default" placeholder="email" />
+    <Input
+      name="password"
+      onChange={cbOnChange}
+      className="password"
+      type="password"
+      size="default"
+      placeholder="password"
+    />
+
+    <Input name="name" onChange={cbOnChange} className="name" type="text" size="default" placeholder="name" />
+    <Input name="surname" onChange={cbOnChange} className="surname" type="text" size="default" placeholder="surname" />
+    <Select
+      onChange={cbOnChangeSelect}
+      className="selectDepartament"
+      placeholder="Select a departament"
+      optionFilterProp="depart"
+    >
+      <Option value="Admin">Admin</Option>
+      <Option value="Doctor">Doctor</Option>
+    </Select>
+  </div>
 );
 
 RegistrationModal.propTypes = {
-    cbOnChangeSelect: PropTypes.func.isRequired
+  cbOnChangeSelect: PropTypes.func.isRequired,
 };
 
 export default RegistrationModal;
