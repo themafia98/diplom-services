365c4cca5216ebe7cda0aeac5ec3f8c2ebe1f37f
Pavel Petrovich <gaara33377@gmail.com>
2020-04-09 12:37:15 +0300

    ie polyfills fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,4 @@
 /** IE supports polyfills */
-import 'core-js/features';
-import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 /** --------------------- */
