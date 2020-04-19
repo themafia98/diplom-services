6b3530ad60e137ba91ef937c1e5e6c7a4e66e5e0
themafia98 <gaara33377@gmail.com>
2020-04-19 16:16:16 +0300

    refac

--- client/src/Redux/actions/publicActions/index.js
+++ client/src/Redux/actions/publicActions/index.js
@@ -1,70 +1,49 @@
 /** Here action constants import */
 import { SET_ERROR, SET_CACHE, SET_STATUS, SHOW_GUIDE, UDATA_LOAD, CLEAR_CACHE, UPDATE_UDATA } from './const';
 
-export /**
- * @param {any} state
- */
-const errorRequstAction = state => {
+export const errorRequstAction = state => {
   return {
     type: SET_ERROR,
     payload: state,
   };
 };
 
-export /**
- * @param {any} state
- */
-const updateUdata = state => {
+export const updateUdata = state => {
   return {
     type: UPDATE_UDATA,
     payload: state,
   };
 };
 
-export /**
- * @param {any} state
- */
-const showGuile = state => {
+export const showGuile = state => {
   return {
     type: SHOW_GUIDE,
     payload: state,
   };
 };
 
-export /**
- * @param {any} state
- */
-const loadUdata = state => {
+export const loadUdata = state => {
   return {
     type: UDATA_LOAD,
     payload: state,
   };
 };
 
-export /**
- * @param {{ data: any; load: boolean; primaryKey: any; }} state
- */
-const сachingAction = state => {
+export const сachingAction = state => {
   return {
     type: SET_CACHE,
     payload: state,
   };
 };
 
-export /**
- * @param {any} state
- */
-const clearCache = state => {
+export const clearCache = state => {
   return {
     type: CLEAR_CACHE,
     payload: state,
   };
 };
 
-export /**
- * @param {{ statusRequst: any; }} state
- */
-const setStatus = state => {
+export const setStatus = state => {
   return {
     type: SET_STATUS,
     payload: state,
--- client/src/Redux/actions/socketActions/index.js
+++ client/src/Redux/actions/socketActions/index.js
@@ -7,60 +7,42 @@ import {
   UPDATE_ENTITY_SOCKET,
 } from './const';
 
-export /**
- * @param {{ listdata?: any[]; tokenRoom: any; isFake?: any; listdataMsgs?: any; shouldLoadingMessage?: boolean; }} state
- */
-const setActiveChatToken = state => {
+export const setActiveChatToken = state => {
   return {
     type: SET_ACTIVE_CHAT_TOKEN,
     payload: state,
   };
 };
 
-export /**
- * @param {{ usersList: any; activeChatRoom?: any; shouldLoadingMessage?: any; listdata: any; options: any; }} state
- */
-const onLoadActiveChats = state => {
+export const onLoadActiveChats = state => {
   return {
     type: LOAD_CHATS_LIST,
     payload: state,
   };
 };
 
-export /**
- * @param {any} state
- */
-const addMsg = state => {
+export const addMsg = state => {
   return {
     type: ADD_CHAT_MSG,
     payload: state,
   };
 };
 
-export /**
- * @param {{ socketConnection: any; activeModule: any; }} state
- */
-const setSocketConnection = state => {
+export const setSocketConnection = state => {
   return {
     type: SET_SOCKET_CONNECTION,
     payload: state,
   };
 };
 
-export /**
- * @param {{ socketConnection: any; msg: any; }} state
- */
-const setSocketError = state => {
+export const setSocketError = state => {
   return {
     type: INVALID_LOAD_SOCKET,
     payload: state,
   };
 };
 
-export /**
- * @param {any} state
- */
-const updateRoom = state => {
+export const updateRoom = state => {
   return {
     type: UPDATE_ENTITY_SOCKET,
     payload: state,
