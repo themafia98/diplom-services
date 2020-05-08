cb0d6ea258e2f21d073848d06591f6403ce4f888
themafia98 <gaara33377@gmail.com>
2020-05-09 02:29:48 +0300

    tags component

--- client/src/Components/TagsContainer/TagsContainer.jsx
+++ client/src/Components/TagsContainer/TagsContainer.jsx
@@ -1,42 +1,49 @@
 // @ts-nocheck
 import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import clsx from 'clsx';
+import { ChromePicker } from 'react-color';
 import { tagsContainerType } from './types';
 import { Input, Tag, Button, Tooltip } from 'antd';
 
 const TagsContainer = (props) => {
   const { shouldVisibleButtonAddTag, modeControll, modeControllEdit } = props;
 
+  const [hex, setHex] = useState('#2db7f5');
+  const [colorVisible, setVisible] = useState(false);
   const [tagValue, setTagValue] = useState('');
-  const [tagList, setTagList] = useState([
-    {
-      color: '#2db7f5',
-      id: Math.random(),
-      tagValue: 'hi',
-    },
-  ]);
+  const [tagList, setTagList] = useState([]);
 
   const onCloseTag = (id) => {
     setTagList(tagList.filter(({ id: tagId = '' }) => tagId !== id));
   };
+
   const onClick = (event) => onKeyDown(event, true);
 
   const onKeyDown = (event, click = false) => {
     event.stopPropagation();
     const { key = '', ctrlKey } = event;
-    if ((key !== 'Enter' && !ctrlKey && !click) || !tagValue) return;
+    if (!click && (key !== 'Enter' || !ctrlKey || !tagValue)) return;
     setTagValue('');
     setTagList([
       ...tagList,
       {
-        color: '#2db7f5',
-        id: Math.random(),
-        tagValue: 'hi',
+        color: hex,
+        id: `virtual__${uuid()}`,
+        sortable: false,
+        tagValue,
       },
     ]);
   };
 
   const onChange = ({ target: { value = '' } }) => {
     setTagValue(value);
+  };
+
+  const onChangeVisible = () => setVisible((prevVisible) => !prevVisible);
+
+  const onChangeColor = ({ hex = '' }) => {
+    setHex(hex);
   };
 
   const isEditable = modeControll === 'edit' && modeControllEdit;
@@ -59,20 +66,26 @@ const TagsContainer = (props) => {
       })}
       {isEditable ? (
         <div className="tags-controllers">
-          <Tooltip
-            placement="top"
-            mouseEnterDelay={1}
-            trigger="hover"
-            title="Что бы добавить новый тег нажмите на комбинацию клавиш Ctrl+Enter"
-          >
-            <Input
-              value={tagValue}
-              onChange={onChange}
-              onKeyDown={onKeyDown}
-              placeholder="добавление новых тегов"
-              type="text"
-            />
-          </Tooltip>
+          <div className="input-controls">
+            <Tooltip
+              placement="top"
+              mouseEnterDelay={1}
+              trigger="hover"
+              title="Что бы добавить новый тег нажмите на комбинацию клавиш Ctrl+Enter"
+            >
+              <Input
+                value={tagValue}
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                placeholder="добавление новых тегов"
+                type="text"
+              />
+            </Tooltip>
+            <Button onClick={onChangeVisible}>Выбрать цвет</Button>
+            <div className={clsx('color-wrapper', colorVisible ? 'visible' : 'hidden')}>
+              <ChromePicker color={hex} onChange={onChangeColor} />
+            </div>
+          </div>
           {shouldVisibleButtonAddTag ? (
             <Button onClick={onClick} type="primary" size="small">
               Добавить
