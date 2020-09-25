cc9079a1c8b7dc58a0f6a341f2deefecfcbdd50a
Pavel Petrovich <gaara33377@gmail.com>
2020-09-25 19:44:06 +0300

    set actions access for roles

--- server/src/app.constant.ts
+++ server/src/app.constant.ts
@@ -1,7 +1,14 @@
-export const enum ACTIONS_ACCESS {
-  EDIT = 'EDIT',
-  VIEW = 'VIEW',
-  READ_VIEW = 'READ-VIEW',
-  CREATE = 'CREATE',
-  DELETE = 'DELETE',
-}
+import { ActionsAccess } from './Utils/Interfaces';
+
+export const ACTIONS_ACCESS: Readonly<ActionsAccess> = {
+  EDIT: 'EDIT',
+  VIEW: 'VIEW',
+  LIMITED_VIEW: 'LIMITED_VIEW',
+  READ_VIEW: 'READ_VIEW',
+  CREATE: 'CREATE',
+  DELETE: 'DELETE',
+};
+
+export const FULL_ACTIONS_ACCESS = Object.values(ACTIONS_ACCESS).filter(
+  (it) => it !== ACTIONS_ACCESS.LIMITED_VIEW && it !== ACTIONS_ACCESS.READ_VIEW,
+);
