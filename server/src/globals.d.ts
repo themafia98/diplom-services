a338e9d0f5920b6141ca2931eba95799b9d73768
Петрович Павел <pipetrovich@itertech.by>
2021-03-04 13:53:45 +0300

    declare jwt module

--- server/src/globals.d.ts
+++ server/src/globals.d.ts
@@ -2,6 +2,7 @@ declare module 'connect-mongo';
 declare module 'express-rate-limit';
 declare module 'uuid';
 declare module 'jsonwebtoken';
+declare module 'passport-jwt';
 declare namespace NodeJS {
   export interface ProcessEnv {
     HOST: string;
