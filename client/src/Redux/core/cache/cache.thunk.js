3073cbe433742f9bb523c8a4b3f80b3ba14d48a3
Петрович Павел <pipetrovich@itertech.by>
2021-03-18 14:35:17 +0300

    resolve async thunks

--- client/src/Redux/core/cache/cache.thunk.js
+++ client/src/Redux/core/cache/cache.thunk.js
@@ -3,7 +3,7 @@ import actionsTypes from 'actions.types';
 import { requestTemplate, paramsTemplate } from 'Utils/Api/api.utils';
 import { setAppCache, setRequestError } from 'Redux/reducers/publicReducer.slice';
 
-const cachingThunk = async (dependencies) => async (dispatch, getState, { schema }) => {
+const cachingThunk = (dependencies) => async (dispatch, getState, { schema }) => {
   const { dataItems = {}, store, actionType, clientDB, updateBy } = dependencies;
 
   try {
@@ -29,7 +29,7 @@ const cachingThunk = async (dependencies) => async (dispatch, getState, { schema
   }
 };
 
-const putterCacheThunk = async (dependencies) => async (dispatch, getState, { schema, Request }) => {
+const putterCacheThunk = (dependencies) => async (dispatch, getState, { schema, Request }) => {
   const { depStore, item = {}, type, uid, actionType, updateBy = '' } = dependencies;
 
   try {
@@ -79,7 +79,7 @@ const putterCacheThunk = async (dependencies) => async (dispatch, getState, { sc
   }
 };
 
-const getterCacheThunk = async (dependencies) => async (dispatch, getState, { schema, Request }) => {
+const getterCacheThunk = (dependencies) => async (dispatch, getState, { schema, Request }) => {
   const { dataItems, actionType, store, clientDB, updateBy } = dependencies;
 
   try {
